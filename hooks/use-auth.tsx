b1d17f3fd83386a624bbb209428169c13720720d
SKAHLO <egbunaoscar@gmail.com>
2025-10-16 11:42:19 +0100

    user authentication

---
+++ hooks/use-auth.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useState, useEffect, createContext, useContext } from "react";
+import { onAuthStateChanged } from "@/lib/firebase/auth";
+
+const AuthContext = createContext({
+  user: null,
+  loading: true,
+});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
