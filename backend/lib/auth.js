705c34ac774591c6737a3a8c195a7219998040a3
SKAHLO <egbunaoscar@gmail.com>
2025-10-22 01:28:26 +0100

    auth.js

---
+++ backend/lib/auth.js
@@ -0,0 +1,28 @@
+const { verifyIdToken } = require("./firebase-admin");
+
+const protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decodedToken = await verifyIdToken(token);
+      req.user = decodedToken;
+      next();
+    } catch (error) {
+      console.error("Error verifying token:", error);
+      res.status(401).json({ error: "Not authorized, token failed" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ error: "Not authorized, no token" });
+  }
+};
+
+module.exports = {
+  protect,
+};
