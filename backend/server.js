ff2a50a7da056d0ea45667bf95e64556498dfb6b
SKAHLO <egbunaoscar@gmail.com>
2025-10-22 01:26:36 +0100

    express.js server setup

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const flowRoutes = require("./routes/flow");
+const onRampRoutes = require("./routes/onramp");
+const offRampRoutes = require("./routes/offramp");
+
+const app = express();
+const port = process.env.BACKEND_PORT || 3001;
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+};
+
+// Middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Routes
+app.use("/api/flow", flowRoutes);
+app.use("/api/onramp", onRampRoutes);
+app.use("/api/offramp", offRampRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Flow Backend is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Backend server listening at http://localhost:${port}`);
+});
