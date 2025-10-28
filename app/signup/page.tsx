7a3f52e75b5587d6eee1736f18afdb5fbd59396a
SKAHLO <egbunaoscar@gmail.com>
2025-10-28 16:29:41 +0100

    signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signUpWithEmail } from "@/lib/firebase/auth";
 import { Button } from "@/components/ui/button";
